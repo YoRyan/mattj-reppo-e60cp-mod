34d6a5a795baf832b7c16523697941df769d1386
Ryan Young <ryan@youngryan.com>
2023-03-14 04:49:35 +0000

    feat: add brake indicator messages with psi reporting

--- src/mod/Assets/Reppo/E60CP/RailVehicles/Scripts/OpenNEC_E60_EngineScript.ts
+++ src/mod/Assets/Reppo/E60CP/RailVehicles/Scripts/OpenNEC_E60_EngineScript.ts
@@ -1,6 +1,9 @@
 /** @noSelfInFile */
 
-require("Assets/Reppo/E60CP/RailVehicles/Scripts/E60_EngineScript.out");
+import * as frp from "lib/frp";
+import { FrpEngine } from "lib/frp-engine";
+import { rejectUndefined } from "lib/frp-extra";
+import * as rw from "lib/railworks";
 
 /**
  * A pulse code frequency combination in use on the Northeast Corridor.
@@ -24,6 +27,56 @@ enum Aspect {
     ApproachLimited = "M11",
     Clear = "M10",
 }
+
+const brakeMessageId = 10101;
+
+// Load and executes the old code. Requires the .out file to be extracted from
+// the asset pack!
+require("Assets/Reppo/E60CP/RailVehicles/Scripts/E60_EngineScript.out");
+// Save the old signal message handler.
+const oldOnCustomSignalMessage = OnCustomSignalMessage;
+
+const me = new FrpEngine(() => {
+    // Universal signal message translator
+    const forwardSignalMessage$ = frp.compose(
+        me.createOnSignalMessageStream(),
+        frp.map(toPulseCode),
+        rejectUndefined(),
+        frp.map(pc => {
+            return {
+                [PulseCode.C_0_0]: Aspect.Restricting,
+                [PulseCode.C_75_0]: Aspect.Approach,
+                [PulseCode.C_75_75]: Aspect.Approach,
+                [PulseCode.C_120_0]: Aspect.ApproachLimited,
+                [PulseCode.C_120_120]: Aspect.ApproachLimited,
+                [PulseCode.C_180_0]: Aspect.Clear,
+                [PulseCode.C_180_180]: Aspect.Clear,
+                [PulseCode.C_270_0]: Aspect.ApproachLimited,
+                [PulseCode.C_270_270]: Aspect.Clear,
+                [PulseCode.C_420_0]: Aspect.ApproachLimited,
+            }[pc];
+        })
+    );
+    forwardSignalMessage$(msg => {
+        oldOnCustomSignalMessage(msg);
+    });
+
+    // Consist message for brake indicator lights
+    const sendBrakeMessage$ = frp.compose(
+        me.createPlayerWithKeyUpdateStream(),
+        frp.throttle(500),
+        frp.map(_ => {
+            const brakeValue = me.rv.GetControlValue("TrainBrakeControl", 0) as number;
+            const psi = me.rv.GetControlValue("AirBrakePipePressurePSI", 0) as number;
+            return string.format("%.3f10101%03d", brakeValue, psi);
+        })
+    );
+    sendBrakeMessage$(msg => {
+        me.rv.SendConsistMessage(brakeMessageId, msg, rw.ConsistDirection.Forward);
+        me.rv.SendConsistMessage(brakeMessageId, msg, rw.ConsistDirection.Backward);
+    });
+});
+me.setup();
 
 /**
  * Attempt to convert a signal message to a pulse code.
@@ -93,23 +146,3 @@ function toPulseCode(signalMessage: string) {
 
     return undefined;
 }
-
-const oldOnSignalMessage = OnCustomSignalMessage;
-OnCustomSignalMessage = msg => {
-    const pulseCode = toPulseCode(msg);
-    if (pulseCode !== undefined) {
-        const emulatedMsg = {
-            [PulseCode.C_0_0]: Aspect.Restricting,
-            [PulseCode.C_75_0]: Aspect.Approach,
-            [PulseCode.C_75_75]: Aspect.Approach,
-            [PulseCode.C_120_0]: Aspect.ApproachLimited,
-            [PulseCode.C_120_120]: Aspect.ApproachLimited,
-            [PulseCode.C_180_0]: Aspect.Clear,
-            [PulseCode.C_180_180]: Aspect.Clear,
-            [PulseCode.C_270_0]: Aspect.ApproachLimited,
-            [PulseCode.C_270_270]: Aspect.Clear,
-            [PulseCode.C_420_0]: Aspect.ApproachLimited,
-        }[pulseCode];
-        oldOnSignalMessage(emulatedMsg);
-    }
-};
